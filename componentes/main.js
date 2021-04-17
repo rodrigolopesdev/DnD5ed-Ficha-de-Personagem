6b1456b5176de508b15d0fdd7742f01c3930251e
Rodrigo Lopes <rodrigolopes.dev@gmail.com>
2021-04-17 16:03:04 -0300

    primeiro protótipo dos modificadores funcionando

--- componentes/main.js
+++ componentes/main.js
@@ -7,6 +7,7 @@ novoAtaque.addEventListener('click', adicionarAtaque)
 const novoItem = document.querySelector('[data-form-button-2]')
 novoItem.addEventListener('click', adicionarItem)
 
+// SEM USO, Arquivar para futura referência
 const colapse = document.getElementById('colapse')
 const navUl = document.getElementById('saving')
 colapse.addEventListener('click', () => {
@@ -15,7 +16,16 @@ colapse.addEventListener('click', () => {
 
 const dots = document.querySelectorAll("span.dot")
 dots.forEach(dot => {
-    dot.addEventListener("click", () => {
+    dot.addEventListener('click', () => {
         dot.classList.toggle('black')
     })
 })
+
+
+FUNCIONANDO
+// const valNum = document.getElementById('inputModificador')
+// valNum.addEventListener('change', () => {
+//     const inputNum = valNum.value
+//     const modCalc = Math.floor((inputNum-10)/2)
+//     document.getElementById('outputModificador').innerHTML=modCalc;
+// })
