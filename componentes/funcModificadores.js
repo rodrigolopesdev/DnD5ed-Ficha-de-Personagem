3825f6af8a6612616cf9916bc3b216fe2a71dc06
Rodrigo Lopes <rodrigolopes.dev@gmail.com>
2021-04-18 02:44:01 -0300

    limp e preparando para novo estágio do projeto

---
+++ componentes/funcModificadores.js
@@ -0,0 +1,29 @@
+function modForca(valNum) {
+    valNum = parseFloat(valNum)
+    document.getElementById("outputForca").innerHTML=Math.floor((valNum-10)/2)
+}
+
+function modDestreza(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputDestreza").innerHTML=Math.floor((valNum-10)/2)
+}
+
+function modSabedoria(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputSabedoria").innerHTML=Math.floor((valNum-10)/2)
+}
+
+function modInteligencia(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputInteligencia").innerHTML=Math.floor((valNum-10)/2)
+}
+
+function modConstituicao(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputConstituicao").innerHTML=Math.floor((valNum-10)/2)
+}
+
+function modCarisma(valNum) {
+    valNum = parseFloat(valNum);
+    document.getElementById("outputCarisma").innerHTML=Math.floor((valNum-10)/2)
+}
--- componentes/main.js
+++ componentes/main.js
@@ -1,6 +1,5 @@
 import adicionarAtaque from './adicionarAtaque.js'
 import adicionarItem from './adicionarItem.js'
-import modificadores from './modificadores.js'
 
 const novoAtaque = document.querySelector('[data-form-button]')
 novoAtaque.addEventListener('click', adicionarAtaque)
@@ -14,22 +13,3 @@ dots.forEach(dot => {
         dot.classList.toggle('black')
     })
 })
-
-const modificador = document.querySelector('[data-mod]')
-modificador.addEventListener('change', modificadores)
-
-
-// SEM USO, Arquivar para futura referência
-// const colapse = document.getElementById('colapse')
-// const navUl = document.getElementById('saving')
-// colapse.addEventListener('click', () => {
-//     navUl.classList.toggle('show')
-// })
-
-// FUNCIONANDO
-// const valNum = document.getElementById('inputModificador')
-// valNum.addEventListener('change', () => {
-//     const inputNum = valNum.value
-//     const modCalc = Math.floor((inputNum-10)/2)
-//     document.getElementById('outputModificador').innerHTML=modCalc;
-// })
